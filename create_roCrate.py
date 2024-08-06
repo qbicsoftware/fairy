785432835b4d8b61e9014bdf5596dbc279377691
Aline-9 <aline.breitinger@student.uni-tuebingen.de>
2024-08-06 13:00:11 +0200

    fix create_roCrate.py

--- create_roCrate.py
+++ create_roCrate.py
@@ -2,14 +2,15 @@
 import os
 from rocrate.rocrate import ROCrate
 
-path_to_metadata = "metadata"
+path_to_metadata = "metadata/"
 
 crate = ROCrate()
 i = 0
-for subdir in os.walk(path_to_metadata):
-    crate.add_dataset(subdir)
+for subdir in os.listdir(path_to_metadata):
+    crate.add_dataset(path_to_metadata + subdir)
     crate.write("roCrates/test_crate" + str(i))
     i += 1
+    crate = ROCrate()
 # metadata = crate.add_file("../metadata-tsv/Q1101.tsv", properties={
 #     "name": "dataset metadata",
 #     "encodingFormat": "text/tab-separated-values"
